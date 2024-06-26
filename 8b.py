b8b2ed3f3d4a221c8b2292fac8099b3c626d0ee4
Chin-Jou, Li <72683971+Angelina354@users.noreply.github.com>
2024-06-26 18:20:55 +0800

    Update 8b.py
    local dataset -> huggingface dataset

--- 8b.py
+++ 8b.py
@@ -1,28 +1,25 @@
 from transformers import pipeline
+from dataset import load_dataset
 import torch
 import pickle
 
-original_texts = "txts/gt.txt"
-erroneous_texts = "txts/error.txt"
-output_file = "outs/8b-res.pkl"
+huggingface_token = "**access token**"
+dataset_type = "short" # or "medium" or "long"
+dataset = load_dataset("json", data_files=f"cjli/finegrained-halu-tw/dataset_{dataset_type}.jsonl", use_auth_token=huggingface_token)
 
-access_token = "**access token**"
+output_file = "outs/7b-res.pkl"
 
+taide_access_token = "**access token**"
 pipe = pipeline("text-generation",
-        token = access_token,
+        token = taide_access_token,
         model = "taide/Llama3-TAIDE-LX-8B-Chat-Alpha1",
         torch_dtype = torch.float16,
         device_map = "auto")
 
-with open(original_texts, 'r') as f:
-    gts = f.readlines()
-with open(erroneous_texts, 'r') as f:
-    errors = f.readlines()
 responses = []
-
-for i in range(0,90):
-    text1 = gts[i]
-    text2 = errors[i]
+for data in dataset:
+    text1 = data["ground_truth"]
+    text2 = data["content_w_error"]
     prompt = f"<s>USER: 你好，請只根據短文一的內容，判斷短文二的正確性。若短文二有誤，請列出違背事實或無法驗證之處。請遵循1. 錯誤一\n 2. 錯誤二\n 的格式直接回應。以下是短文一 : 「{text1}」以下是短文二 : 「{text2}」ASSISTANT:"
     out = pipe(prompt, max_new_tokens=512)
     response = out[0]["generated_text"].split("ASSISTANT:")[1]
