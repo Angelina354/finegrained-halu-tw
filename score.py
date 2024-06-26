d613ee36fbc62260a09a8502ca84dbe0eea7737d
Chin-Jou, Li <72683971+Angelina354@users.noreply.github.com>
2024-06-26 18:55:45 +0800

    Update score.py

--- score.py
+++ score.py
@@ -1,12 +1,17 @@
 import sys
+from dataset import load_dataset
 
 modelname = sys.argv[1] # 7b, 8b or breeze
-type_of_all_errors = "txts/wrongtype.txt"
 
-with open(f'eval-{modelname}.txt', 'r') as f:
+huggingface_token = "**access token**"
+dataset_type = "short" # or "medium" or "long"
+dataset = load_dataset("json", data_files=f"cjli/finegrained-halu-tw/dataset_{dataset_type}.jsonl", use_auth_token=huggingface_token)
+
+labels = []
+for data in dataset:
+    for error_type in data["errors_type"]: labels.append(error_type) # collect all errors_types together
+with open(f'outs/eval-{modelname}.txt', 'r') as f:
     error_texts = f.readlines()
-with open(type_of_all_errors, 'r') as f:
-    labels = f.readlines()
 
 # errorcode: noun 1, relation 2, sentence 3 -> factual errors
 #           imagined 4, comment 5, unknown 6 -> unverifiable errors
@@ -16,10 +21,8 @@ cnt = 0
 for line in error_texts:
     if "Yes" in line or "No" in line:
         errorcode = int(labels[cnt])
-        if "Yes" in line: #error found
-            result[errorcode-1].append(1)
-        else: # error missed
-            result[errorcode-1].append(0)
+        if "Yes" in line: result[errorcode-1].append(1)    # error found
+        else: result[errorcode-1].append(0)                # error missed
         cnt += 1
     else: continue
 
@@ -37,4 +40,3 @@ print("3- sent acc:", sum(result[2]), '/', len(result[2]), ';', sum(result[2])/l
 print("4- imag acc:", sum(result[3]), '/', len(result[3]), ';', sum(result[3])/len(result[3]))
 print("5- comm acc:", sum(result[4]), '/', len(result[4]), ';', sum(result[4])/len(result[4]))
 print("6- unkn acc:", sum(result[5]), '/', len(result[5]), ';', sum(result[5])/len(result[5]))
-
